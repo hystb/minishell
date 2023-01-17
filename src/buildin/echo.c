c0a2303dbc37300d10181343faf1fb2763411fe6
Ethaniel Billon <ebillon@z2r4p6.42lyon.fr>
2023-01-17 16:58:55 +0100

    working on echo

--- src/buildin/echo.c
+++ src/buildin/echo.c
@@ -1,19 +1,13 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   minishell.h                                        :+:      :+:    :+:   */
+/*   echo.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ebillon <ebillon@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/01/16 15:39:36 by ebillon           #+#    #+#             */
-/*   Updated: 2023/01/17 16:51:04 by ebillon          ###   ########lyon.fr   */
+/*   Created: 2023/01/17 16:50:38 by ebillon           #+#    #+#             */
+/*   Updated: 2023/01/17 16:58:43 by ebillon          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef MINISHELL_H
-# define MINISHELL_H
-
-/* do some stuff there */
-# include "../libft/libft.h"
-
-#endif
+#include "../../../include/"
