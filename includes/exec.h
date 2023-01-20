700b5dfdca748b344541a858711d452ab97660eb
Ethaniel Billon <ebillon@z2r1p3.42lyon.fr>
2023-01-20 15:42:12 +0100

    manage here_doc

---
+++ includes/exec.h
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   exec.h                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ebillon <ebillon@student.42lyon.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/20 13:21:11 by ebillon           #+#    #+#             */
+/*   Updated: 2023/01/20 14:16:12 by ebillon          ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef EXEC_H
+# define EXEC_H
+/* about pipex */
+# include <unistd.h>
+# include <stddef.h>
+# include <stdlib.h>
+# include <string.h>
+# include <stdio.h>
+# include <fcntl.h>
+# include <errno.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+# include <sys/wait.h>
+# include "../libft/libft.h"
+
+void	exit_error(void);
+void	write_error(char *str);
+void	not_found_error(char *cmd);
+void	do_execute(char **args, char **env, int *tube);
+void	do_here_doc(char *limiter);
+char	*get_path(char *cmd, char **env);
+
+/* some stuff here */
+#endif 
