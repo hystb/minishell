df629e73c8a00a5c94e2880ab2b1be9ac8a58c57
Ethaniel  Billon <ethaaalpha@Je-suis-une-plante.local>
2023-02-19 21:44:48 +0100

    update

--- src/pipex/redirections_cases.c
+++ src/pipex/redirections_cases.c
@@ -1,49 +1,44 @@
 #include "../../includes/exec.h"
 
-int	do_redirection(char **cmds, t_redirect *data)
+int	do_redirection(char **args, t_redirect *data)
 {
 	int	value;
 
 	value = 0;
-	value += do_redir_in(cmds, data);
-	value += do_redir_out(cmds, data);
+	value += do_redir_in(args, data);
+	value += do_redir_out(args, data);
 	return (value);
 }
 
-int do_redir_in(char **cmds, t_redirect *data)
+int do_redir_in(char **args, t_redirect *data)
 {
-	if (ft_strncmp(cmds[0], "<<", 2) == 0)
+	if (ft_strncmp(args[0], "<<", 2) == 0)
 	{
-		do_heredoc(cmds[1]);
+		do_heredoc(args[1]);
 		// wait(NULL);
 		return (1);
 	}
-	else if (ft_strncmp(cmds[0], "<", 1) == 0)
+	else if (ft_strncmp(args[0], "<", 1) == 0)
 	{
 		// if (do_input(cmds[1]))
 			// return (2);
-		do_input(cmds[1], data);
+		do_input(args[1], data);
 		return (1);
 	}
 	return (0);
 }
 
-int do_redir_out(char **cmds, t_redirect *data)
+int do_redir_out(char **args, t_redirect *data)
 {
-	if (ft_strncmp(cmds[0], ">>", 2) == 0)
+	if (ft_strncmp(args[0], ">>", 2) == 0)
 	{
-		do_writing_file(data->tube_out, cmds[1], 1);
+		do_writing_file(data->tube_out, args[1], 1);
 		return (1);
 	}
-	else if (ft_strncmp(cmds[0], ">", 1) == 0)
+	else if (ft_strncmp(args[0], ">", 1) == 0)
 	{
-		do_writing_file(data->tube_out, cmds[1], 0);
+		do_writing_file(data->tube_out, args[1], 0);
 		return (1);
 	}
 	return (0);
-}
-
-void check_redir_exec(char **cmds, t_redirect *data)
-{
-	
 }
