97ab0b8a238bc404622b854a43da0952cd41b51e
Ethaniel  Billon <ethaaalpha@Je-suis-une-plante.local>
2023-02-21 11:35:09 +0100

    working version without handling pipes

--- exec/do_exec.c
+++ exec/do_exec.c
@@ -13,34 +13,42 @@
 #include "../includes/exec.h"
 	// void	pre_redirect(char **cmds, int argc, char **env, t_redirect *data)
 
+void	read_result(int fd)
+{
+	char buff[2];
+
+	if (fd > 0)
+	{
+		while (read(fd, buff, 1))
+			write(1, buff, 1);
+		close(fd);
+	}
+}
+
 void	do_exec(t_list **lst_cmd, char **env)
 {
 	t_redirect	data;
+	int			status;
 
 	data.fd = -1;
 	data.tube_out = -1;
 	data.pid = -1;
 	pre_redirect(*lst_cmd, ft_lstsize(*lst_cmd), env, &data);
-	// lire dans le tube de sortie.
+	waitpid(data.pid, &status, 0);
+	read_result(data.fd);
 }
 
-void	do_commands(char **cmds, char **env)
-{
-	// int			status;
-	t_redirect  data;
+// void	do_commands(char **cmds, char **env)
+// {
+// 	// int			status;
+// 	// t_redirect  data;
 
-	(void) cmds;
-	(void) env;
-	data.fd = -1;
-	// data.tube_out = -1;
-	// pre_redirect(cmds, 3, env, &data);
-	// printf("pid: %d | fd: %d \n", data.pid, data.fd);
-	//waitpid(data.pid, &status, 0);
-	char buff[2];
-	if (data.fd > 0)
-	{
-		while (read(data.fd, buff, 1))
-			write(1, buff, 1);
-		close(data.fd);
-	}
-}
+// 	(void) cmds;
+// 	(void) env;
+// 	data.fd = -1;
+// 	// data.tube_out = -1;
+// 	// pre_redirect(cmds, 3, env, &data);
+// 	// printf("pid: %d | fd: %d \n", data.pid, data.fd);
+// 	// waitpid(data.pid, &status, 0);
+
+// }
