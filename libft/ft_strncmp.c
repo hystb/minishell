ed86c3289fd90367ff5570bdd273c50bbdeaab65
Ethaniel Billon <ebillon@z4r7p1.42lyon.fr>
2023-03-07 14:16:13 +0100

    malloc protection

--- libft/ft_strncmp.c
+++ libft/ft_strncmp.c
@@ -3,13 +3,13 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strncmp.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ebillon <marvin@42.fr>                     +#+  +:+       +#+        */
+/*   By: ebillon <ebillon@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/08 16:41:46 by ebillon           #+#    #+#             */
-/*   Updated: 2022/11/08 16:42:10 by ebillon          ###   ########lyon.fr   */
+/*   Updated: 2023/03/07 14:15:42 by ebillon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-#include "libft.h"
+
 
 /*
 Compare *s1 and *s2 and return *s1 - *s2 when reach end of file or different.
@@ -18,6 +18,8 @@ Compare *s1 and *s2 and return *s1 - *s2 when reach end of file or different.
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
 	if (n == 0)
+		return (0);
+	if (!s1 || !s2)
 		return (0);
 	while (*s1 == *s2 && n - 1 && *s1 && *s1)
 	{
