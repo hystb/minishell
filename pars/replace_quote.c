c74ed9c659dcf685909db45024a302db9f75dc4b
hystb <milan.nicolas@rocketmail.com>
2023-03-15 13:00:34 +0100

    change quote

--- pars/replace_quote.c
+++ pars/replace_quote.c
@@ -25,7 +25,7 @@ void	replace_quote(t_data var_lst)
 		while (tmp->content != NULL && tmp->content[i])
 		{
 			new_content = is_quote(tmp->content[i]);
-			if (new_content)
+			if (ft_strncmp(new_content, tmp->content[i], ft_strlen(tmp->content[i])))
 			{
 				free(tmp->content[i]);
 				tmp->content[i] = new_content;
@@ -41,6 +41,7 @@ char	*is_quote(char *in)
 	char	c;
 	int		i;
 	int		index;
+	char	*tmp;
 
 	c = 0;
 	i = 0;
@@ -55,11 +56,14 @@ char	*is_quote(char *in)
 				index = i;
 			}
 			else if (in[i] == c)
-				return (ft_strjoin(ft_substr(in, 0, index), \
-				ft_strjoin(ft_substr(in, index + 1, i - 1), \
-				ft_substr(in, i + 1, ft_strlen(&in[i + 1])))));
+			{
+				in = ft_strjoin(ft_substr(in, 0, index), ft_strjoin(\
+				ft_substr(in, index + 1, i - 1 - index), ft_substr(in, i + 1, ft_strlen(in))));
+				c = 0;
+				i = i - 2;
+			}
 		}
 		i++;
 	}
-	return (NULL);
+	return (in);
 }
